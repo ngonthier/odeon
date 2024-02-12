c8417504381621a01a874bde7b834bd0192ad020
ngonthier <nicolas.gonthier@ign.fr>
2024-02-12 17:15:29 +0100

    ajout de clearml

--- xp/dchan_hiucd.py
+++ xp/dchan_hiucd.py
@@ -20,6 +20,7 @@ import albumentations as A
 from pytorch_lightning.loggers.logger import DummyLogger
 from mlflow_logger import MLFlowLogger
 import mlflow
+from clearml import Task
 
 import logging
 
@@ -46,6 +47,8 @@ def build_params(stage: str, root_dir: str, batch_size: int, num_workers: int, t
 def main():
 
     debug = True
+    clearml = True
+    mlflow = False
 
     possible_roots = [r'\\store\store-DAI\datasrc\dchan', r'C:\Users\NGonthier\Documents\Detection_changement\data', 
                       "/mnt/stores/store-DAI/datasrc/dchan", "/var/data/datasets"]
@@ -118,27 +121,32 @@ def main():
         train_img_size = 32
 
 
-    mlflow_experiment_name = "dchan_hiucd"
-    if 'MLFLOW_EXPERIMENT_NAME' in os.environ:
-        mlflow_experiment_name = os.environ['MLFLOW_EXPERIMENT_NAME']        
+    if mlflow:
+        mlflow_experiment_name = "dchan_hiucd"
+        if 'MLFLOW_EXPERIMENT_NAME' in os.environ:
+            mlflow_experiment_name = os.environ['MLFLOW_EXPERIMENT_NAME']        
 
-    print("Initializing mlflow connection...")
-    # mlflow_tracking_uri = "http://smlpmlftenap1.ign.fr:5000"
-    # mlflow_tracking_uri = "http://smlpslurmmft1.ign.fr:8000"
-    mlflow_tracking_uri = None
-    if 'MLFLOW_TRACKING_URI' in os.environ:
-        mlflow_tracking_uri = os.environ['MLFLOW_TRACKING_URI']       
+        print("Initializing mlflow connection...")
+        # mlflow_tracking_uri = "http://smlpmlftenap1.ign.fr:5000"
+        # mlflow_tracking_uri = "http://smlpslurmmft1.ign.fr:8000"
+        mlflow_tracking_uri = None
+        if 'MLFLOW_TRACKING_URI' in os.environ:
+            mlflow_tracking_uri = os.environ['MLFLOW_TRACKING_URI']       
 
-    mlflow_save_dir = None
-    if 'MLFLOW_SAVE_DIR' in os.environ:
-        mlflow_save_dir = os.environ['MLFLOW_SAVE_DIR']       
+        mlflow_save_dir = None
+        if 'MLFLOW_SAVE_DIR' in os.environ:
+            mlflow_save_dir = os.environ['MLFLOW_SAVE_DIR']       
 
-    artifact_location = None #"file:/mnt/stores/store-DAI/pocs/slurm/dchan/mlruns"
-    if 'MLFLOW_ARTIFACT_LOCATION' in os.environ:
-        artifact_location = os.environ['MLFLOW_ARTIFACT_LOCATION']       
+        artifact_location = None #"file:/mnt/stores/store-DAI/pocs/slurm/dchan/mlruns"
+        if 'MLFLOW_ARTIFACT_LOCATION' in os.environ:
+            artifact_location = os.environ['MLFLOW_ARTIFACT_LOCATION']     
 
+    if clearml:
+        clearml_experiment_name = "dchan_hiucd"
+        if 'CLEARML_EXPERIMENT_NAME' in os.environ:
+            clearml_experiment_name = os.environ['CLEARML_EXPERIMENT_NAME']
 
-    tensorboard_location = "/mnt/stores/store-DAI/pocs/slurm/dchan/tensorboard"
+    tensorboard_location = "/mnt/stores/store-DAI/pocs/slurm/dchan/tensorboard_ng"
     if not os.path.exists(tensorboard_location) or debug:
         tensorboard_location = 'tensorboard'
     if 'TENSORBOARD_LOCATION' in os.environ:
@@ -182,15 +190,19 @@ def main():
     if 'TRAIN_SEED' in os.environ:
         seed = int(os.environ['TRAIN_SEED'])
 
-    print("mlflow_experiment_name", mlflow_experiment_name)
-    print("mlflow_run_name", name)
+    if mlflow:
+        print("mlflow_experiment_name", mlflow_experiment_name)
+        print("mlflow_run_name", name)
+        print("mlflow_tracking_uri", mlflow_tracking_uri)
+        print("mlflow_save_dir", mlflow_save_dir)
+        print("artifact_location", artifact_location)
+    if clearml:
+        print("clearml_experiment_name", clearml_experiment_name)
+        print("clearml_run_name", name)
     print("lr", train_lr)
     print("train_model", train_model)
     print("batch_size", batch_size)
     print("encoder name", encoder_name)
-    print("mlflow_tracking_uri", mlflow_tracking_uri)
-    print("mlflow_save_dir", mlflow_save_dir)
-    print("artifact_location", artifact_location)
     print("tensorboard_location", tensorboard_location)
     print("has_transform", has_transform)
     print("has_log", has_log)
@@ -213,28 +225,36 @@ def main():
     loggers = [DummyLogger()]
     tags = {}
 
-    if has_log:        
-        mlflow.set_tracking_uri(mlflow_tracking_uri)
+    if has_log:      
+        if mlflow:  
+            mlflow.set_tracking_uri(mlflow_tracking_uri)
 
-        from mlflow.tracking.context import registry as context_registry
+            from mlflow.tracking.context import registry as context_registry
 
-        tags = context_registry.resolve_tags({})
-        print("Tags", tags)            
+            tags = context_registry.resolve_tags({})
+            print("Tags", tags)            
 
-        mlflow_logger = MLFlowLogger(
-            experiment_name=mlflow_experiment_name,
-            tracking_uri=mlflow_tracking_uri,
-            save_dir=mlflow_save_dir,
-            run_name=name,
-            log_model=True,
-            # tags=tags,
-            artifact_location=artifact_location
-        )
+            mlflow_logger = MLFlowLogger(
+                experiment_name=mlflow_experiment_name,
+                tracking_uri=mlflow_tracking_uri,
+                save_dir=mlflow_save_dir,
+                run_name=name,
+                log_model=True,
+                # tags=tags,
+                artifact_location=artifact_location
+            )
+
+        if clearml:
+            task = Task.init(project_name=clearml_experiment_name, task_name='ClearML logging')
+            #clearml_logger = task.get_logger()
         
         os.makedirs(os.path.join(path_tb_log, mlflow_experiment_name, name), exist_ok=True)
         logger = pl_loggers.TensorBoardLogger(save_dir=path_tb_log, name=os.path.join(mlflow_experiment_name, name))
 
-        loggers = [logger, mlflow_logger]
+        if mlflow:
+            loggers = [logger, mlflow_logger]
+        else:
+            loggers = [logger]
 
     print("Initializing Seeds...")
 
@@ -357,14 +377,30 @@ def main():
     trainer = Trainer(logger=loggers, callbacks=callbacks, **trainer_args)
 
     if trainer.global_rank == 0 and has_log:
-        mlflow_logger.log_hyperparams({"batch_size": batch_size})
-        
-        for k,v in tags.items():
-            mlflow_logger.experiment.set_tag(mlflow_logger._run_id, k, v)
 
-        for k,v in trainer_args.items():
-            mlflow_logger.experiment.set_tag(mlflow_logger._run_id, k, v)
-        
+        if mlflow:
+            mlflow_logger.log_hyperparams({"batch_size": batch_size})
+            
+            for k,v in tags.items():
+                mlflow_logger.experiment.set_tag(mlflow_logger._run_id, k, v)
+
+            for k,v in trainer_args.items():
+                mlflow_logger.experiment.set_tag(mlflow_logger._run_id, k, v)
+            
+        if clearml:
+            task: Task = Task.init(
+                        project_name=mlflow_experiment_name,
+                        task_name=job_name
+                    )
+            clearm_tags = ["slurm", "jean-zellou", dataset, model_type, encoder_name]
+            if encoder_weights is not None:
+                clearm_tags.append(encoder_weights)
+            task.add_tags(clearm_tags)
+            task.set_configuration_object("fit_params", config_type="datamodule", config_dict=fit_params)
+            task.set_configuration_object("val_params", config_type="datamodule", config_dict=val_params)
+            task.set_configuration_object("test_params", config_type="datamodule", config_dict=test_params)
+            task.set_configuration_object("trainer_args", config_type="trainer", config_dict=trainer_args)
+
         print("Trainer Args", trainer_args)
 
     trainer.fit(model=model, datamodule=input)
